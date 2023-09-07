19831922c842047c973a327d7ebd0204ccab52f3
Binadam442 <sanusiadam0000@gmail.com>
2023-09-07 05:52:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts function
+ * Return:0
+ */ 
+int main(void)
+{
+putts("\"Programming is like building a multilingual puzzl
+e");
+return (0);
+}
