7fbab722188408710754f58cc557462dbd4cc535
Binadam442 <sanusiadam0000@gmail.com>
2023-09-18 10:43:19 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,21 @@
-#include<stdio.>
+nclude <stdio.h>
 
 /**
- ** _strlen - Returns the length of a string
- ** @s: String to count
- **
- ** Return: String length
- **/
+ *  * _strlen - returns the length of a string
+ *   * @s: string to evaluate
+ *    *
+ *     * Return: the length of the string
+ *      */
 int _strlen(char *s)
 {
-	int c = 0;
+		int i;
 
-	for (; *s != '\0'; s++)
-		{
-	c++;
-									}
+			i = 0;
 
-	return (c);
+				while (s[i] != '\0')
+						{
+									i++;
+										}
+
+					return (i);
 }
